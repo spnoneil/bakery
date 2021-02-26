1d2a58b1381247eef7db7bf4b1ea0a0f671fc902
Scott O'Neil <scottieoneil@gmail.com>
2021-02-26 13:17:45 -0800

    continue ui

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,7 +9,7 @@ namespace Bakery
     {
       Console.WriteLine("----Welcome to Pierre's Bakery!----");
       Console.WriteLine("We got Bread, we got Pastry!");
-      Console.WriteLine("You want Bread, and/or Pastry? (Y/N)");
+      Console.WriteLine("You want? (Y/N)");
       string yOrN = (Console.ReadLine()).ToLower();
       if (yOrN == "y")
       {
@@ -17,11 +17,35 @@ namespace Bakery
       string userPurchaseChoice = (Console.ReadLine()).ToLower();
       if (userPurchaseChoice == "bread")
       {
-        Console.WriteLine("Good news! We've got a special on bread today; buy 2, get 1 free!");
+        Console.WriteLine("Good news! We've got a special on bread today: buy 2, get 1 free!");
         Console.WriteLine("How many you want?!");
-        int breadAmount = int.Parse(Console.ReadLine());
-        // if (breadAmount == int)
-
+        string breadAmountInput = Console.ReadLine();
+        int breadAmount;
+        if (int.TryParse(breadAmountInput, out breadAmount))
+        {
+          Console.Write("parse");
+        }
+        else
+        {
+          Console.WriteLine("Sorry, we're looking for a numeric amount. Please try again!");
+          //ENTER BACK TO BREAD METHOD
+        }
+      else if (userPurchaseChoice == "pastry")
+      {
+        Console.WriteLine("Great choice! Pastries around here go for $2 each, but today we've got a special: buy 2, get the 3rd half off!");
+        Console.WriteLine("How many can we interest you in?");
+        string pastryAmountInput = Console.ReadLine();
+        int pastryAmount;
+        if (int.TryParse(pastryAmountInput, out pastryAmount))
+        {
+          Console.Write("parse");
+        }
+        else
+        {
+          Console.WriteLine("Sorry, we're looking for a numeric amount. Please try again!");
+          //ENTER BACK TO BREAD METHOD
+        }
+      }
       }
       }
       else if (yOrN == "n")
@@ -31,6 +55,7 @@ namespace Bakery
       else
       {
         Console.WriteLine("Sorry, I didn't catch that. Try again, please!");
+        Console.ReadLine();
         Main();
       }
     }
