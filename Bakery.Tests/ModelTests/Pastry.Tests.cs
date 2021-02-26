4c268b22c4e96cd791823a13464430897e3cf29b
Scott O'Neil <scottieoneil@gmail.com>
2021-02-26 10:08:32 -0800

    pass pastry special test

--- Bakery.Tests/ModelTests/Pastry.Tests.cs
+++ Bakery.Tests/ModelTests/Pastry.Tests.cs
@@ -12,5 +12,12 @@ namespace Bakery.Test
       Pastry newPastry = new Pastry(4, 5);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
+    [TestMethod]
+    public void PastrySpecial_ReturnsDiscountedPastryAmount_Int()
+    {
+      Pastry newPastry = new Pastry(4, 5);
+      int result = newPastry.PastrySpecialDiscount(20);
+      Assert.AreEqual(6, result);
+    }
   }
 }
--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -12,5 +12,20 @@ namespace Bakery.Models
       Price = price;
       PurchaseAmount = purchaseAmount;
     }
+
+    public int PastrySpecialDiscount(int userInput)
+    {
+      int discounted = 0;
+      if (userInput % 3 == 0)
+      {
+        discounted = userInput / 3;
+      }
+      else
+      {
+        int extra = userInput % 3;
+        discounted = (userInput - extra) / 3;
+      }
+      return discounted;
+    }
   }
 }
