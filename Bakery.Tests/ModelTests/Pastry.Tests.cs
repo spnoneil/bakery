7833d2866f34ab59db39f173f15c7fafb225be33
Scott O'Neil <scottieoneil@gmail.com>
2021-02-26 15:55:07 -0800

    add additional pastry tests

--- Bakery.Tests/ModelTests/Pastry.Tests.cs
+++ Bakery.Tests/ModelTests/Pastry.Tests.cs
@@ -13,6 +13,21 @@ namespace Bakery.Test
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
     [TestMethod]
+    public void GetPastryOrder_ReturnPastryOrder_Int()
+    {
+      int pastryTest = 5;
+      Pastry newPastry = new Pastry(5);
+      int result = newPastry.Amount;
+      Assert.AreEqual(pastryTest, result);
+    }
+    [TestMethod]
+    public void PastrySpecialCost_ReturnRegularPastryCost_Int()
+    {
+      Pastry newPastry = new Pastry(1);
+      int result = newPastry.PastrySpecialCost(newPastry.Amount);
+      Assert.AreEqual(2, result);
+    }
+    [TestMethod]
     public void PastrySpecial_ReturnsDiscountedPastryCost_Int()
     {
       Pastry newPastry = new Pastry(4);
